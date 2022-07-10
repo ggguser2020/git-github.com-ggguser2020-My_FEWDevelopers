ae92f4138fea82dd7cc0de5c33aadbe70ef5a39a
ggguser2020 <lzhxsi@hotmail.com>
2022-07-10 21:34:47 +0800

    111

--- 课程练习/HTML_css/TP/j1.js
+++ 课程练习/HTML_css/TP/j1.js
@@ -5,31 +5,4 @@
 //   }
 // }
 // f1();  //输出  “f1被调用”
-// let xx=f1();  //将f1函数赋给xx,且return 数据给xx
-// xx();  //f1被调用，f1被调用，f2被调用
-// xxx=1;
-// console.log(xxx);
-//1、下方代码在JS中可单独运行，
-let y=true;
-function myConfirm(){
-    if (y) {
-        y="T";
-    }else {
-        y="取消";
-    }
-    console.log(y);
-}
-myConfirm();
-console.log(y);
-
-//2但在html中写成下方样式才能运行
-function myConfirm(){
-    let y=true;
-    if (y) {
-        y="T";
-    }else {
-        y="取消";
-    }
-    console.log(y);
-}
-myConfirm();
+// let xx
--- 课程练习/HTML_css/TP/ts1.ts
+++
@@ -1,7 +0,0 @@
-
-function compact(arr) {
-    if (arr.length > 10) {
-        return arr.trim(0, 10);
-    }
-        return arr;
-}
