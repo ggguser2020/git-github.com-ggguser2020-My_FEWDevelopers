47e0f2028441fd8c4909c954c02261b106eb16d0
ggguser2020 <lzhxsi@hotmail.com>
2022-09-22 16:49:05 +0800

    Changes

---
+++ VS2022/lesson_1/src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+let msg_a="abcdsfdsaf"
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+  <br>  
+
+
+
+
+
+
+  <main>
+    <!-- <TheWelcome /> -->
+    <div  class="hh">
+      <h1>{{msg_a}}</h1>
+    </div>
+    
+  </main>
+</template>
+
+<style scoped>
+  .hh{
+    width: 400px;
+    height: 400px;
+    align-content: center;
+
+    margin: 600px,0,0,0;
+  }
+  .hhh{
+    display: block;
+  }
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
